d3459f559d46fefe22c4b13e532fe2550adbf33d
wx-chevalier <384924552@qq.com>
2025-12-19 16:08:59 +0800

    feat: update articles

--- 99~参考资料/Python-100-Days/Day01-15/code/Day14/chatclient.py
+++ 99~参考资料/Python-100-Days/Day01-15/code/Day14/chatclient.py
@@ -13,22 +13,22 @@ def main():
         def run(self):
             while running:
                 data = self._client.recv(1024)
-                print(data.decode('utf-8'))
+                print(data.decode("utf-8"))
 
-    nickname = input('请输入你的昵称: ')
+    nickname = input("请输入你的昵称: ")
     myclient = socket()
-    myclient.connect(('10.7.189.118', 12345))
+    myclient.connect(("10.7.189.118", 12345))
     running = True
     RefreshScreenThread(myclient).start()
     while running:
-        content = input('请发言: ')
-        if content == 'byebye':
-            myclient.send(content.encode('utf-8'))
+        content = input("请发言: ")
+        if content == "byebye":
+            myclient.send(content.encode("utf-8"))
             running = False
         else:
-            msg = nickname + ': ' + content
-            myclient.send(msg.encode('utf-8'))
+            msg = nickname + ": " + content
+            myclient.send(msg.encode("utf-8"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
